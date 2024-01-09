3f5fe05a3f6933bba255d0747d950f1c2be40651
luiswebneo <luisfernandolopes0@gmail.com>
2024-01-09 09:30:12 -0300

    Finalizado parte de preços

--- src/components/Snack/index.tsx
+++ src/components/Snack/index.tsx
@@ -1,5 +1,6 @@
 import { FiPlus} from 'react-icons/fi';
 import { Container } from './styles';
+import { currencyFormat } from '../../helpers/currencyFormat';
 
 interface SnacksProps {
   snacks: any[]
@@ -8,19 +9,21 @@ interface SnacksProps {
 export function Snacks({ snacks }: SnacksProps){
   return (
     <Container>
-      {snacks.map((snack) => (
-        <div key={snack.id} className='snack'>
-          <h2>{snack.name}</h2>
-          <img src={snack.image} alt={snack.name} />
-          <p>{snack.description}</p>
-          <div>
-        <strong>{snack.price}</strong>
-        <button type='button'>
-          <FiPlus />
-        </button>
-        </div>
-        </div>
-      ))}
+      {snacks.map((snack) => {
+        return (
+          <div key={snack.id} className='snack'>
+            <h2>{snack.name}</h2>
+            <img src={snack.image} alt={snack.name} />
+            <p>{snack.description}</p>
+            <div>
+              <strong>{currencyFormat(snack.price)}</strong>
+              <button type='button'>
+                <FiPlus />
+              </button>
+            </div>
+          </div>
+        );
+      })}
     </Container>
 
   )
