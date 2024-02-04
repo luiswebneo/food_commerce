e1d8b60bd6db355c178d4eb6019025c7e1a88e8b
luiswebneo <luisfernandolopes0@gmail.com>
2024-02-03 21:23:47 -0300

    Começando no na pagina do carrinho

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, ReactNode, useState } from "react";
-
+import { toast } from "react-toastify";
 import { SnackData } from "../interfaces/SnackData";
+import { snackEmoji } from "../helpers/snackEmoji";
 
 interface Snack extends SnackData {
   quantity: number;
@@ -50,8 +51,7 @@ export function CartProvider({ children }: CartProviderProps) {
         }
         return item;
       });
-
-      console.log(`newCart atualização`, newCart);
+      toast.success(`Outro(a) ${snackEmoji(snack.snack)} ${snack.name} adicionado nos pedidos`)
       setCart(newCart);
 
       return;
@@ -61,8 +61,7 @@ export function CartProvider({ children }: CartProviderProps) {
     const newSnack = { ...snack, quantity: 1, subtotal: snack.price };
     const newCart = [...cart, newSnack]; // push de um array
 
-    console.log(`newCart`, newCart);
-
+    toast.success(`Outro(a) ${snackEmoji(snack.snack)} ${snack.name} adicionado nos pedidos`)
     setCart(newCart);
   }
 
