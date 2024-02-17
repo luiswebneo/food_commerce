a25f8f89849ab3d7a00bd07a5279b241dde105bc
luiswebneo <luisfernandolopes0@gmail.com>
2024-02-16 22:04:10 -0300

    Finalizado cart

--- src/pages/MyCart/Table/index.tsx
+++ src/pages/MyCart/Table/index.tsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react";
 import { useCart } from "../../../hooks/useCart";
 
 import { TableDesktop } from "./TableDesktop";
+import { TableMobile } from "./TableMobile";
+import { EmptyCart } from "../../../components/EmptyCart";
 
 export function Table() {
+  const [windowWidth, setwindowWidth] = useState(document.documentElement.clientWidth)
+
   const { cart } = useCart()
 
-  if(cart.length === 0) return <h1>Ops! Parece que você não tem pedidos, peça já! </h1>
+  useEffect(() => {
+    function updateTableComponentBaseInWindowWidth(){
+      const currentWidth = document.documentElement.clientWidth
+      setwindowWidth(currentWidth)
+    }
+    window.addEventListener('resize', updateTableComponentBaseInWindowWidth)
 
-  return <TableDesktop />
+    return () => {
+      window.removeEventListener('resize', updateTableComponentBaseInWindowWidth)
+    }
+  }, [])
+
+  if(cart.length === 0) return <EmptyCart title='Ops! Parece que você não tem pedidos, peça já!' />
+
+  return windowWidth > 768 ?  <TableDesktop /> : <TableMobile />
 }
